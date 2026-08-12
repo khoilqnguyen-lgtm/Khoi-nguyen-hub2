71f665d954de130992d86f98998011e401810131
khoilqnguyen-lgtm <khoilqnguyen@gmail.com>
2026-08-12 19:48:53 +0700

    Update Khoi_new_keyui.lua

--- Khoi_new_keyui.lua
+++ Khoi_new_keyui.lua
@@ -1,1 +1,11 @@
-print("Khoi Nguyen Hub Da Load Thanh Cong")
+local KeySystemUI = loadstring(game:HttpGet("https://raw.githubusercontent.com/khoilqnguyen-lgtm/KeyUI/main/keysystem.lua"))()
+
+KeySystemUI:Init({
+	Title = "khoinguyenhub.com",
+	Description = "Key Vĩnh Viễn 30k - Ib Khôi Nguyễn",
+	KeyLink = "https://link4m.com/abcxyz", -- ĐỔI LINK BÁN KEY CỦA BẠN
+	CorrectKey = "", -- ĐỂ TRỐNG
+	SaveKey = true,
+	SaveFile = "System32_Data.bin",
+	Logo = "rbxassetid://1535438414453172",
+})
